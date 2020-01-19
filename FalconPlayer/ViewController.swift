1f61974066a97811bcda08a2575db62224ed8815
aseo <aseo@yahoo-corp.jp>
2020-01-20 02:26:51 +0900

    プレイヤーログのうち、一部の色が間違っていたので統一

--- FalconPlayer/ViewController.swift
+++ FalconPlayer/ViewController.swift
@@ -59,13 +59,13 @@ class ViewController: UIViewController {
         
         let videoPlayerView = Bundle.main.loadNibNamed("VideoPlayerView", owner: self, options: nil)?.first as! VideoPlayerView
         videoPlayerView.frame = playerView.frame
-        insertLog(category: "frame = ", categoryColor: UIColor.orange, value: "frame: \(videoPlayerViewFrame)")
+        insertLog(category: "frame = ", categoryColor: UIColor.systemOrange, value: "frame: \(videoPlayerViewFrame)")
         // https://multiplatform-f.akamaihd.net/i/multi/will/bunny/big_buck_bunny_,640x360_400,640x360_700,640x360_1000,950x540_1500,.f4v.csmil/master.m3u8
         videoPlayerView.delegate = self
         videoPlayerView.setVideoURL(url: videoURL)
         insertLog(category: "setVideoURL(url:)", categoryColor: UIColor.systemOrange, value: videoURL!.absoluteString)
         playerView.addSubview(videoPlayerView)
-        insertLog(category: "playerView.addSubview", categoryColor: UIColor.orange)
+        insertLog(category: "playerView.addSubview", categoryColor: UIColor.systemOrange)
         
 
         videoPlayerView.translatesAutoresizingMaskIntoConstraints = false
