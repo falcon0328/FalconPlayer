1eaae2fc02a003ad2e35d9bf067417f194cbe190
aseo <aseo@yahoo-corp.jp>
2020-01-19 23:55:21 +0900

    ログを表示するテーブルビューを実装

---
+++ FalconPlayer/VideoPlayerLogTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  VideoPlayerLogTableViewCell.swift
+//  FalconPlayer
+//
+//  Created by aseo on 2020/01/19.
+//  Copyright © 2020 Falcon Tech. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerLogTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        dateLabel.text = ""
+        categoryLabel.text = ""
+        valueLabel.text = ""
+    }
+    
+}
--- FalconPlayer/ViewController.swift
+++ FalconPlayer/ViewController.swift
@@ -10,12 +10,48 @@ import UIKit
 import AVFoundation
 import CoreGraphics
 
+struct VideoPlayerLog {
+    /// 日時
+    let date = Date()
+    /// 種類
+    let category: String
+    /// 値
+    let value: String
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
     var videoPlayerView: VideoPlayerView?
+    
+    var videoPlayerLogList: [VideoPlayerLog] = []
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    var dateFormatter: DateFormatter {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        dateFormatter.locale = Locale(identifier: "ja_JP")
+        return dateFormatter
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let log1 = VideoPlayerLog(category: "category1", value: "aaa")
+        let log2 = VideoPlayerLog(category: "category1", value: "aaa\nbbb")
+        let log3 = VideoPlayerLog(category: "category1", value: "aaa\nbbb\nccc")
+        videoPlayerLogList = [log1, log2, log3]
+        
+        tableView.estimatedRowHeight = 58
+        tableView.register(UINib(nibName: "VideoPlayerLogTableViewCell", bundle: Bundle.main),
+                           forCellReuseIdentifier: "VideoPlayerLogTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -53,4 +89,22 @@ extension ViewController: VideoPlayerViewDelegate {
     func didChange(videoPlayerView: VideoPlayerView, audioState: AudioState) {}
     
     func didTap(videoPlayerView: VideoPlayerView, componentName: String) {}
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videoPlayerLogList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "VideoPlayerLogTableViewCell", for: indexPath) as! VideoPlayerLogTableViewCell
+        cell.dateLabel.text = dateFormatter.string(from: videoPlayerLogList[indexPath.row].date)
+        cell.categoryLabel.text = videoPlayerLogList[indexPath.row].category
+        cell.valueLabel.text = videoPlayerLogList[indexPath.row].value
+        return cell
+    }
 }
